4cf91d7df95c58a77a481d742eea85ab88f96aa3
huangw-d <mylovexiaowei@gmail.com>
2015-09-06 16:53:23 +0800

    二分查找算法

--- Recursion.py
+++ Recursion.py
@@ -16,3 +16,25 @@ def power(x, n):
        return 1
    else:
        return x * power(x, n-1)
+
+
+# 二分法查找
+def search(sequence, number, lower=0, upper=None):
+    if upper is None:
+        upper = len(sequence) - 1
+    if lower == upper:
+        # assert语句用来判断某个条件是否为真，为假时会引发AssertionError异常
+        assert number == sequence[upper]
+        return upper
+    else:
+        middle = (lower + upper) // 2
+        if number > sequence[middle]:
+            return search(sequence, number, middle + 1, upper)
+        else:
+            return search(sequence, number, lower, middle)
+
+
+seq = [34, 67, 8, 123, 5, 100, 98]
+seq.sort()
+print seq
+print search(seq, 34, 0, 7)
